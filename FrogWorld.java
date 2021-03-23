69433a44ec540965270a419afb2d0a5ebf202533
Jillian Maher <jillian.maher2@boone.kyschools.us>
2021-03-23 09:39:26 -0400

    Initial commit

---
+++ FrogWorld.java
@@ -0,0 +1,24 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Roadway for the Frog Jump game.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class FrogWorld extends World
+{
+
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public FrogWorld()
+    {    
+        // Create a new world with 800x600 cells with a cell size of 1x1 pixels.
+        super(800, 600, 1); 
+        
+        
+    }
+    
+}
